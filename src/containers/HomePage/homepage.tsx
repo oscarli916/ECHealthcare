610bb1037176176917b06aadac0f4a27ac011f7e
Oscar Li <oscar_li916@yahoo.com.hk>
2022-02-14 17:12:54 +0800

    feat: updatetime trailing zero

--- src/containers/HomePage/homepage.tsx
+++ src/containers/HomePage/homepage.tsx
@@ -28,7 +28,9 @@ const HomePage = () => {
           setData(resData);
           let today = new Date();
           setUpdateTime(
-            `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+            `${("0" + today.getHours()).slice(-2)}:${(
+              "0" + today.getMinutes()
+            ).slice(-2)}:${("0" + today.getSeconds()).slice(-2)}`
           );
         });
     }, 1000);
